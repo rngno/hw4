0a6fc741637fd4b4f74698b2a35f94f55c14b025
MJ Deus <mdeus@usc.edu>
2025-11-14 19:45:18 -0800

    testing out bst insert and remove in avlbst to see where errors are

--- avlbst.h
+++ avlbst.h
@@ -148,7 +148,53 @@ protected:
 template<class Key, class Value>
 void AVLTree<Key, Value>::insert (const std::pair<const Key, Value> &new_item)
 {
-    // TODO
+    // TODO testing if insert() from bst has issue
+
+    // if tree is empty, we can just insert at root and skip the rest
+    if(root_ == NULL) {
+        root_ = new Node<Key, Value>(keyValuePair.first, keyValuePair.second, NULL);
+        return;
+    }
+
+    Node<Key, Value>* curr = root_; 
+
+    // in this loop, walk thru tree to find if key already exists or where to insert new node
+    while(true) {
+        // key already exists, so just update value and return
+        if(keyValuePair.first == curr->getKey()) {
+            curr->setValue(keyValuePair.second);
+            return;
+        }
+
+        // key doesn't exist, so time to start walking
+
+        // key is less than current, so go left
+        else if(keyValuePair.first < curr->getKey()) {
+            // no left child, so we can insert to left
+            if(curr->getLeft() == NULL) {
+                Node<Key, Value>* newNode = new Node<Key, Value>(keyValuePair.first, keyValuePair.second, curr); // REMEMBER TO FREE
+                curr->setLeft(newNode);
+                return; // placed node, so we can end here
+            }
+            // left child exists, so take it from top of loop
+            else {
+                curr = curr->getLeft();
+            }
+        }
+        // key is greater than current, so go right
+        else {
+            // no right child, so we can insert to right
+            if(curr->getRight() == NULL) {
+                Node<Key, Value>* newNode = new Node<Key, Value>(keyValuePair.first, keyValuePair.second, curr); // REMEMBER TO FREE
+                curr->setRight(newNode);
+                return; // placed node, so we can end here
+            }
+            // right child exists, so take it from top of loop
+            else {
+                curr = curr->getRight();
+            }
+        }
+    }
 }
 
 /*
@@ -158,7 +204,57 @@ void AVLTree<Key, Value>::insert (const std::pair<const Key, Value> &new_item)
 template<class Key, class Value>
 void AVLTree<Key, Value>:: remove(const Key& key)
 {
-    // TODO
+    // TODO testing if remove from bst has issue
+
+    // error catch if key not in tree already
+    Node<Key, Value>* nodeToRemove = internalFind(key);
+    if(nodeToRemove == nullptr) {
+        return; // key not found
+    }
+
+    // check to see if nodeToRemove has 2 kids, if so then we swap until there's only <=1 kid associated with it 
+    while (nodeToRemove->getLeft() != nullptr && nodeToRemove->getRight() != nullptr) {
+        Node<Key, Value>* pred = predecessor(nodeToRemove);
+        nodeSwap(nodeToRemove, pred); // swap so nodeToRemove will still point at the node we want to nuke but placed in pred spot
+    }
+
+    // populate a child node pointer with either the leftmost child or, after we run out of left kids, the right child
+    Node<Key, Value>* child = (nodeToRemove->getLeft() != nullptr) ? nodeToRemove->getLeft() : nodeToRemove->getRight(); // this syntax is so much better looking than the if statements that would take its place tbh
+
+    Node<Key, Value>* parent = nodeToRemove->getParent();
+
+    // ohhh my god the nesting is so ugly here
+    // if there's no parent node (i.e. we're at root) then nuke root
+    if (parent == nullptr) {
+        root_ = child;
+
+        // child becomes new root node if the rest of the tree isn't empty
+        if (child != nullptr){
+            child->setParent(nullptr);
+        }
+    }
+
+    // there is a parent node so we let a child take parent's place
+    else {
+        // can use given getters and setters declared in class 
+        // node to be nuked is on the left of our parent node, so use child to take its place
+        if (parent->getLeft() == nodeToRemove) {
+            parent->setLeft(child);
+        } 
+
+        // node to be nuked is on the right of our parent node, so use child to take its place
+        else {
+            parent->setRight(child);
+        }
+
+        // preserve tree structure by having child's parent become parent node
+        if (child != nullptr){
+            child->setParent(parent);
+        }
+    }
+
+    delete nodeToRemove;
+
 }
 
 template<class Key, class Value>
